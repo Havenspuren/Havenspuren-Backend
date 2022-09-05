e38566223600fb44b82d981b29fe7337657e69c9
omKano100 <90765305+omKano100@users.noreply.github.com>
2022-09-05 16:39:20 +0200

    Image upload

--- app/Http/Controllers/WriteController.php
+++ app/Http/Controllers/WriteController.php
@@ -6,6 +6,8 @@ use App\Http\Resources\SimpleRoute;
 use Illuminate\Http\Request;
 use App\Models\Route;
 use Validator;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 
 class WriteController extends Controller
@@ -26,17 +28,27 @@ class WriteController extends Controller
             'path_to_character_image' => 'required'
         ]);
 
+        $imageName = Str::random(32).".".$request->path_to_character_image->getClientOriginalExtension();
+
         $route = Route::create([
             'name' => $request->name,
             'description' => $request->description,
             'path_to_route_image' => $request->path_to_route_image,
             'expected_time' => $request->expected_time,
             'path_to_map_image' => $request->path_to_map_image,
-            'path_to_character_image' => $request->path_to_character_image
-     
+            //'path_to_character_image' => $request->path_to_character_image
+            'path_to_character_image' => $imageName,
          ]);
+         
+         //save Image in Storag folder
+         Storage::disk('public')->put($imageName, file_get_contents($request->path_to_character_image));
 
         return new SimpleRoute($route);
+
+        //return Json Response
+        return response()->json([
+            'message' => 'Route successfully created.'
+        ], 200);
     }
 
     public function updateRoute(Request $request, $routeId)
